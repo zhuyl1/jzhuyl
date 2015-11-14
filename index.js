eccbe0e3c23907379f3beeeb84a5f72acd85b119
yulong10 <yulong10@staff.sina.com.cn>
2015-11-14 09:22:18 +0800

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require("http");
 //test
 http.createServer(function(request, response) {  
     response.writeHead(200, {"Content-Type": "text/plain"});  
-    response.write("hello nodetest71 -2015-11-23-15:30");  
+    response.write("hello nodetest71 -2016-02-17-15:30");  
     response.end();
 }).listen(5050);
 
