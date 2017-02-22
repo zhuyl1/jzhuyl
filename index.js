5773a658e4b793d3b0fda50b52e8f1d1292ba756
zhuyl1 <379385955@qq.com>
2017-02-22 18:57:39 +0800

    ff
    ff

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});  
     response.write("hello nodetest72 -2016-11-23-15:30");  
     response.end();
-}).listen(5051);
+}).listen(5050);
 
 console.log("nodejs start listen 5050 port!");
