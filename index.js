eb299cb26d0ddf8dfd37c44babe4b91817b1d096
zhuyl1 <379385955@qq.com>
2017-02-22 17:30:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});  
     response.write("hello nodetest72 -2016-11-23-15:30");  
     response.end();
-}).listen(5050);
+}).listen(5051);
 
 console.log("nodejs start listen 5050 port!");
